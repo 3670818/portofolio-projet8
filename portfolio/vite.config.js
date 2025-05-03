2a64701541666168adf71e3d3f84e30408a798b5
allae <ibrahimallae1@gmail.com>
2025-05-03 21:21:28 +0200

    ajout du root

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -11,6 +11,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'portfolio', // Cela définit le répertoire principal de ton application
   plugins: [react()],
   base: '/portofolio-projet8/', // Assure-toi que c'est bien le chemin sur GitHub Pages
   assetsInclude: ['**/*.PNG', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'], // Ajout des types d'images
