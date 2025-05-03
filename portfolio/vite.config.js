d57d76cb63520560767f2ac78324b45e01c17e72
allae <ibrahimallae1@gmail.com>
2025-05-04 00:16:50 +0200

    supressions des changements

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -9,16 +9,15 @@
 
 // vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'portfolio', // Cela définit le répertoire principal de ton application
+  root: 'portfolio', // Répertoire principal de ton application
   plugins: [react()],
-  base: '/portofolio-projet8/', // Assure-toi que c'est bien le chemin sur GitHub Pages
-  assetsInclude: ['**/*.PNG', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'], // Ajout des types d'images
+  base: process.env.NODE_ENV === 'production' ? '/portofolio-projet8/' : '/', // Ajuste le chemin en fonction de l'environnement
+  assetsInclude: ['**/*.PNG', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'], // Types d'images à inclure
   build: {
     outDir: 'dist',  // Dossier de sortie pour les fichiers de production
   },
 });
-
 
