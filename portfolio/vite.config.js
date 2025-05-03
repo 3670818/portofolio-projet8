9a1b2929f40d8c35d3c81b0b11eb46470302e973
allae <ibrahimallae1@gmail.com>
2025-05-03 23:09:38 +0200

    test

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -6,6 +6,7 @@
 //   assetsInclude: ['**/*.PNG', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
 // });
 
+
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
